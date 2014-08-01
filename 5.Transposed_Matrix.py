cad9207332988c876728fc83d4f088162f608dc5
RaviTeja <rpothan@corp.untd.com>
2014-08-01 18:03:38 +0530

    In progress

---
+++ 5.Transposed_Matrix.py
@@ -0,0 +1,34 @@
+#/usr/bin/python
+
+def checkio(data):
+   """ Takes in a matrix(tuple of lists)
+       and returns Matrix transpose(tuple of lists)"""
+   rows    = len(data)
+   columns = len(data[0])
+   trans = []
+   for fill in xrange(rows):
+       print fill
+       cur = []
+       for seq in data:
+           print "seq["+str(fill)+"]"
+           cur.append(seq[fill])
+       trans.append(cur)
+   return trans       
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]]) == [[1, 4, 7],
+                                    [2, 5, 8],
+                                    [3, 6, 9]], "Square matrix"
+    assert checkio([[1, 4, 3],
+                    [8, 2, 6],
+                    [7, 8, 3],
+                    [4, 9, 6],
+                    [7, 8, 1]]) == [[1, 8, 7, 4, 7],
+                                    [4, 2, 8, 9, 8],
+                                    [3, 6, 3, 6, 1]], "Rectangle matrix"
+
+
