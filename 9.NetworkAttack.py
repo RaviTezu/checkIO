111035967e78e19bee2c35a14f260b978f835fbc
ravitezu <ravi-teja@live.com>
2014-08-04 23:39:46 +0530

    In progress 9.NetworkAttack.py

---
+++ 9.NetworkAttack.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+def capture(matrix):
+    """ Network attack, returns the time taken to shit all the systems!"""
+    net = {}
+    num_systems = len(matrix)
+    for sys in range(num_systems):
+        cur_row = matrix[sys]
+        dumb = []
+        for k,v in enumerate(cur_row):
+             if v == 1:
+                 dumb.append(k)
+        net[sys] = dumb
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert capture([[0, 1, 0, 1, 0, 1],
+                    [1, 8, 1, 0, 0, 0],
+                    [0, 1, 2, 0, 0, 1],
+                    [1, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 1, 3, 1],
+                    [1, 0, 1, 0, 1, 2]]) == 8, "Base example"
+    assert capture([[0, 1, 0, 1, 0, 1],
+                    [1, 1, 1, 0, 0, 0],
+                    [0, 1, 2, 0, 0, 1],
+                    [1, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 1, 3, 1],
+                    [1, 0, 1, 0, 1, 2]]) == 4, "Low security"
+    assert capture([[0, 1, 1],
+                    [1, 9, 1],
+                    [1, 1, 9]]) == 9, "Small"
