1d6e619ba6740bb6a7dddce157daf65a9bb43584
RaviTezu <ravi-teja@live.com>
2014-08-05 14:09:00 +0530

    Done with NetoworkAttack

--- 9.NetworkAttack.py
+++ 9.NetworkAttack.py
@@ -4,6 +4,12 @@ def capture(matrix):
     """ Network attack, returns the time taken to shit all the systems!"""
     net = {}
     num_systems = len(matrix)
+    #Counts number of systems
+    totalsys = [x for x in range(num_systems)]
+    #Take out the daigonal from matrix
+    timeneed = [matrix[i][i] for i in xrange(num_systems)]
+    #Creates a dict. with system number as key and connected
+    #systems as values in a list
     for sys in range(num_systems):
         cur_row = matrix[sys]
         dumb = []
@@ -11,7 +17,22 @@ def capture(matrix):
              if v == 1:
                  dumb.append(k)
         net[sys] = dumb
-
+    #print net
+    done = [0]
+    inpro = [n for n in net[0]]
+    time  = 0
+    #Loop until all systems are cracked.
+    while sum(timeneed)!=0: 
+        for node in inpro:
+            timeneed[node] = timeneed[node] - 1 
+        time = time + 1
+        for i,j in enumerate(timeneed):
+            if j == 0:
+                if i in inpro:
+                    inpro.remove(i)
+                    done.append(i)
+                    inpro = list(set(inpro + net[i])-set(done))
+    return time
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert capture([[0, 1, 0, 1, 0, 1],
