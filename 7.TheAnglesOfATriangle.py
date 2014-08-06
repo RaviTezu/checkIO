2e1388efe11b1a98c2c019072ceecd0bc608f30a
RaviTezu <ravi-teja@live.com>
2014-08-06 15:33:25 +0530

    Explicitly import each module

--- 7.TheAnglesOfATriangle.py
+++ 7.TheAnglesOfATriangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
-import math
+from math import degrees, acos
 
 def checkio(a, b, c):
     """ Takes 3 sides of a triangle and return the 3 angles as a list for valid triangle sides"""
     #checking for valide trangle
     if a + b > c and b + c > a and c + a > b:
         #Calculate the angeles Lets say [A, B, C] 
-        C = round(math.degrees(math.acos((a**2 + b**2 - c**2)/float((2*a*b)))))
-        B = round(math.degrees(math.acos((a**2 + c**2 - b**2)/float((2*a*c)))))
+        C = round(degrees(acos((a**2 + b**2 - c**2)/float((2*a*b)))))
+        B = round(degrees(acos((a**2 + c**2 - b**2)/float((2*a*c)))))
         A = 180 - (B + C)
         return sorted([A, B, C])
     else:
